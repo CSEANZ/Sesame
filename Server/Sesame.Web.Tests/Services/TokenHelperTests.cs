618c67fa7d7b2f6d8a8873a370530fb70fe1d88b
Jordan Knight <jakkaj@gmail.com>
2018-01-05 14:36:12 +1100

    Removed client ids

--- Server/Sesame.Web.Tests/Services/TokenHelperTests.cs
+++ Server/Sesame.Web.Tests/Services/TokenHelperTests.cs
@@ -15,10 +15,10 @@ namespace Sesame.Web.Tests
         [TestMethod]
         public async Task Validate_ValidKey_ExpectAuthenticationResult()
         {
-            string authority = "https://login.microsoftonline.com/d05d5e5b-385d-4774-b496-d0cf85bfa5f4/";
-            string clientId = "860e612a-5e72-41ac-a6b5-70f432be8bbf";
+            string authority = "https://login.microsoftonline.com/<Your tenant id>/";
+            string clientId = "<your client id>";
             string resource = "https://graph.microsoft.com/";
-            string userId = "4832e139-8e1d-4083-b0ef-e9888411c42f";
+            string userId = "<your user id>";
 
             IDistributedCache distributedCache = new FileCache();
 
